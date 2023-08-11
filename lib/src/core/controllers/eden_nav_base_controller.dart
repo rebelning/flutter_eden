ea6e6de6e3949ab286eaf496de832782977d8aa0
rebelning <shaxiaoan@gmail.com>
2023-08-11 10:56:19 +0800

    fix nav base controller

---
+++ lib/src/core/controllers/eden_nav_base_controller.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_eden/eden.dart';
+
+abstract class EdenNavBaseController extends EdenBaseController {
+  final PageController _pageController = PageController();
+  PageController get pageController => _pageController;
+  int _currentIndex = 0;
+  int get currentIndex => _currentIndex;
+
+  void onNavTap(int index) {
+    _currentIndex = index;
+    _handleOnChange(index);
+    update();
+  }
+
+  void _handleOnChange(int index) {
+    _pageController.jumpToPage(index);
+  }
+}
