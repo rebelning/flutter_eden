2ef35c4f8e209678639a2ce4649119273a049ff6
yujianbin <yujianbin@xgrobotics.com>
2024-07-25 09:08:05 +0800

    update readme

--- lib/src/values/locale/eden_app_translations.dart
+++ lib/src/values/locale/eden_app_translations.dart
@@ -5,16 +5,16 @@ import 'package:flutter_eden/eden.dart';
 class EdenAppTranslations extends Translations {
   final Map<String, Map<String, String>> _translations = {};
 
-  Map<String, String> get en_Keys => {
-        'loading': 'loading...',
-        "uploaded": "upload completed",
-        "uploading": "uploading",
-      };
-  Map<String, String> get zh_Keys => {
-        'loading': '加载中...',
-        "uploaded": "上传完成",
-        "uploading": "上传中",
-      };
+  // Map<String, String> get en_Keys => {
+  //       'loading': 'loading...',
+  //       "uploaded": "upload completed",
+  //       "uploading": "uploading",
+  //     };
+  // Map<String, String> get zh_Keys => {
+  //       'loading': '加载中...',
+  //       "uploaded": "上传完成",
+  //       "uploading": "上传中",
+  //     };
 
   Future<void> loadTranslations(String locale) async {
     Map<String, String>? tran = _translations[locale];
